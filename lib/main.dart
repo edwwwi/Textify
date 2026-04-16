874d7c7f138083eabbdc51bec60cbf6af977b84b
Edwin Joy <161407507+edwwwi@users.noreply.github.com>
2026-04-16 09:28:53 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'screens/image_to_text.dart';
 import 'screens/text_read_aloud.dart';
 import 'screens/youtube_caption.dart';
 import 'home_screen.dart';
-
+///////////////////////////
 void main() {
   runApp(TextifyApp());
 }
